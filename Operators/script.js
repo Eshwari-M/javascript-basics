f23ec5955ba381eab9f49634c52f71059cec9b55
Eshwari-M <eshwarim22@gmail.com>
2026-08-04 17:08:10 +0530

    DAY 2

---
+++ Operators/script.js
@@ -0,0 +1,27 @@
+let empName='Dahlia'
+let salary=35000
+console.log(empName,salary +' is the salary of '+empName);
+salary=salary+5000
+console.log(empName," salary is increased to ",salary);
+
+//operators - symbols used to perform some operations.
+
+//1. Arithmetic Operators - +, -, *, /, %, ++, --, **
+console.log("Multiplication: ", salary*5);
+let team1=2;
+console.log("Power of:", team1**3);
+
+let emp1=20;
+console.log("Quotient: ",emp1/10);
+
+let emp2=20;
+console.log("Remainder: ",emp2%10);
+console.log("Salary :",salary);
+salary=salary+10000
+console.log("Salary :",salary);
+
+//2. Assignment Operators - +=, -=, *=, /=, %=, **=
+salary+=10000;
+console.log("Salary :",salary);
+salary-=5000;
+console.log("Salary :",salary);
