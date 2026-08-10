f85bee766f6bb567b88846929eb4e6033a06f336
Eshwari-M <eshwarim22@gmail.com>
2026-08-10 17:59:36 +0530

    JS Update

--- Date/script.js
+++ Date/script.js
@@ -5,3 +5,4 @@ console.log(today.getDate());
 console.log(today.getDay()); //days start from 0 to 6
 console.log(today.getFullYear());
 console.log(today.getMonth()); //months start from 0 to 11
+let joiningDate=new Date("2026-08-07");
